1f1adab10040586fb017b9910288dd027275bed0
Jan Stránský <jan.stransky@arnal.cz>
2016-04-22 05:16:51 +0200

    Trading in your menubar ;-)

---
+++ bux.1m.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# <bitbar.title>BUX</bitbar.title>
+# <bitbar.version>v0.1</bitbar.version>
+# <bitbar.author>Jan Stránský</bitbar.author>
+# <bitbar.author.github>burningtree</bitbar.author.github>
+# <bitbar.desc>Show BUX trading overview.</bitbar.desc>
+# <bitbar.image></bitbar.image>
+
+JQ="/usr/local/bin/jq"
+
+# get access token
+ACCESS_TOKEN=`cat ~/.bux-config.json | ${JQ} .account.access_token | sed 's/"//g'`
+
+# fetch data from users/me/portfolio/performance
+DATA=`curl -s -H "X-App-Version: 1.36-2697" -H "Accept-Language: en" -H "Authorization: Bearer ${ACCESS_TOKEN}" -H "Host: api.getbux.com" -H "User-Agent: okhttp/3.2.0" https://api.getbux.com/core/13/users/me/portfolio/performance`
+
+# match values
+PERF=`echo "${DATA}" | ${JQ} .performance | sed 's/"//g'`%
+VALUE=`echo "${DATA}" | ${JQ} .accountValue.amount | sed 's/"//g'`
+CURRENCY=`echo "${DATA}" | ${JQ} .accountValue.currency | sed 's/"//g'`
+
+# print
+echo $PERF
+echo "---"
+echo "Balance: ${VALUE} ${CURRENCY}"
